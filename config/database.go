11d3e4f2441cd81c642174b0747a0a49780f4b96
fauzan <fauzan.prasetyatama@dataon.com>
2026-04-11 14:58:29 +0700

    update db 1

--- config/database.go
+++ config/database.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+    "fmt"
+    "os"
+
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
 )
@@ -8,11 +11,19 @@ import (
 var DB *gorm.DB
 
 func ConnectMasterDB() {
-    dsn := "user:password@tcp(host:port)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+    host := os.Getenv("MYSQLHOST")
+    port := os.Getenv("MYSQLPORT")
+    user := os.Getenv("MYSQLUSER")
+    password := os.Getenv("MYSQLPASSWORD")
+    dbname := os.Getenv("MYSQLDATABASE")
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+        user, password, host, port, dbname,
+    )
 
     database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
-        panic("failed to connect database")
+        panic("failed to connect database: " + err.Error())
     }
 
     DB = database
