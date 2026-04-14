4726a9002ec9ecc337aad89e0d45f0ed79213b8a
fauzan <fauzan.prasetyatama@dataon.com>
2026-04-14 09:40:52 +0700

    patches 3.6.24

--- services/auth_service.go
+++ services/auth_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
     "errors"
+    "fmt"
     "hris/config"
     "hris/models"
 
@@ -17,7 +18,7 @@ func Login(accountCode, username, password string) (*models.User, *models.Accoun
     }
 
     var user models.User
-    if err := db.Where("username = ? AND account_id = ?", username, account.ID).First(&user).Error; err != nil {
+    if err := db.Where("username = ? AND account_id = ?", strings.ToLower(username), account.ID).First(&user).Error; err != nil {
         return nil, nil, err
     }
 
@@ -26,6 +27,14 @@ func Login(accountCode, username, password string) (*models.User, *models.Accoun
     if err != nil {
         return nil, nil, errors.New("invalid password")
     }
+
+    fmt.Println("INPUT:")
+    fmt.Println("accountCode:", accountCode)
+    fmt.Println("username:", username)
+    fmt.Println("password:", password)
+
+    fmt.Println("DB USER:", user.Username)
+    fmt.Println("DB PASSWORD:", user.Password)
 
     return &user, &account, nil
 }
