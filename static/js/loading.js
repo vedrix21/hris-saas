fe3d3065b34b033dee1460ebf569f93f04ac162d
fauzan <fauzan.prasetyatama@dataon.com>
2026-04-15 14:09:04 +0700

    patches 3.6.28

--- static/js/loading.js
+++ static/js/loading.js
@@ -10,3 +10,13 @@ function hideLoading() {
     const loader = document.getElementById("globalLoading");
     if (loader) loader.style.display = "none";
 }
+
+function handleNav(e, url) {
+    e.preventDefault();
+
+    showLoading("Opening...");
+
+    setTimeout(() => {
+        window.location.href = url;
+    }, 300); // delay biar animasi keliatan
+}
