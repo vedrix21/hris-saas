9c35cc7bb71d762739212f59ddb7714462ebd282
Fauzan <fauzanakbarpr@gmail.com>
2026-05-14 06:33:41 +0700

    41

--- controllers/dashboard_controller.go
+++ controllers/dashboard_controller.go
@@ -26,7 +26,7 @@ func Dashboard(c *gin.Context) {
 	daysLeft := int(account.SubscriptionEnd.Sub(time.Now()).Hours() / 24)
 
 	fmt.Printf("Account ID : %d\n", account.ID)
-	fmt.Print("Username : ", user.Username)
+	fmt.Print("Username : \n", user.Username)
 
 	var userdb models.User
 	err := config.DB.Where("account_id = ? and username = ?", account.ID, user.Username).First(&userdb)
