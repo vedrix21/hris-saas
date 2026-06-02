b46b4a6b4c3038e2bd82b20880d0c1560b9f4caa
fauzan <fauzan.prasetyatama@dataon.com>
2026-06-02 10:16:02 +0700

    66

--- controllers/setting_controller.go
+++ controllers/setting_controller.go
@@ -15,6 +15,21 @@ func MigrationPage(c *gin.Context) {
 
 	menus := services.GetSidebar(user.Role, tenant)
 
+	var account models.Account
+	config.DB.Where("code = ?", tenant).First(&account)
+	daysLeft := int(account.SubscriptionEnd.Sub(time.Now()).Hours() / 24)
+
+	var userdb models.User
+	err := config.DB.Where("account_id = ? and username = ?", account.ID, user.Username).First(&userdb)
+	if err != nil {
+		fmt.Printf("Error fetching user from DB: %v\n", err)
+	}
+
+	warning := false
+	if daysLeft <= 7 {
+		warning = true
+	}
+
 	utils.Render(c, []string{
 		"templates/settings/data_migration.html",
 	}, gin.H{
@@ -24,5 +39,8 @@ func MigrationPage(c *gin.Context) {
 		"Role":        user.Role,
 		"CurrentPath": c.Request.URL.Path,
 		"User":        user,
+		"SubscriptionWarning": warning,
+		"DaysLeft":            daysLeft,
+		"UserDB":              userdb,
 	})
 }
